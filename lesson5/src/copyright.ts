d39937f535774d4171a1fe5e63c90ac41859d66c
John Roberts <bigjlr2004@yahoo.com>
2023-07-03 10:11:11 -0500

    Finished Lesson 5

--- lesson5/src/copyright.ts
+++ lesson5/src/copyright.ts
@@ -4,8 +4,18 @@
 // year.setAttribute("datetime", thisYear)
 // year?.textContent = thisYear
 
-// 1st variation
-const year = document.getElementById("year");
-const thisYear = new Date().getFullYear();
+// // 1st variation
+// let year : HTMLElement | null
+// year = document.getElementById("year");
+// let thisYear: string
+// thisYear = new Date().getFullYear().toString();
+// if (year){
+//     year.setAttribute("datetime", thisYear);
+//     year.textContent = thisYear;
+// }
+
+// 2nd variation
+const year = document.getElementById("year") as HTMLSpanElement;
+const thisYear: string = new Date().getFullYear().toString();
 year.setAttribute("datetime", thisYear);
-year?.textContent = thisYear;
+year.textContent = thisYear;
